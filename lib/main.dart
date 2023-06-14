596feb9157a6960e96153aeeb0e9bbf2b1fa76b4
champaksworldcreate <champaksworld@gmail.com>
2023-06-14 10:17:27 +0530

    Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      
       home: Scaffold(
         backgroundColor: Colors.blueAccent,
         appBar: AppBar(
-
           centerTitle: true,
-          title:  Text("Varanasi Software Junction"),
+          title: const Text("Varanasi Software Junction"),
           backgroundColor: Colors.blueGrey,
         ),
-
-        body:
-
-        Center(child: Image(
-          image: NetworkImage(
-              "https://3.bp.blogspot.com/-py5FbTZgvjo/YDi1bsQq16I/AAAAAAAACB0/BxejbJBcHA4AVfkB33WYC3YlVmxElM7BwCK4BGAYYCw/s1600/Varanasi%2BSoftware%2BJunction%2BPhone%2BLogo.png"),
-        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                "https://3.bp.blogspot.com/-py5FbTZgvjo/YDi1bsQq16I/AAAAAAAACB0/BxejbJBcHA4AVfkB33WYC3YlVmxElM7BwCK4BGAYYCw/s1600/Varanasi%2BSoftware%2BJunction%2BPhone%2BLogo.png"),
+          ),
         ),
       ),
     ),
